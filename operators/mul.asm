d575d1eb46d39f6d95b12801436855f1cc80e177
Roberto Loza <robertoloxa88@gmail.com>
2025-05-13 13:07:40 -0600

    fix mul

--- operators/mul.asm
+++ operators/mul.asm
@@ -1,40 +1,47 @@
 section .data
-    num1 dd 5 
-    num2 dd 3  
-    msg db "El resultado es: ", 0xA 
-    len equ $ - msg  
+    num1 db 5      
+    num2 db 4   
+    msg db "resultado = "
+    len_msg equ $ - msg
+    newline db 0xA 
 
 section .bss
-    resultado resb 2
+    resultado resb 3 ;
 
 section .text
     global _start
 
 _start:
-    ; Multiplicación (eax = num1 * num2)
-    mov eax, [num1] 
-    mov ebx, [num2]
-    mul ebx          ; Multiplica eax por ebx, resultado en eax
-    
-    ; Convertir resultado a ASCII
-    add eax, '0'   
-    mov [resultado], eax 
-    mov byte [resultado+1], 0xA  ; Salto de línea
-    
-    ; Mostrar mensaje
-    mov eax, 4      ; sys_write
-    mov ebx, 1      ; stdout
-    mov ecx, msg    ; mensaje
-    mov edx, len    ; longitud
+   
+    mov al, [num1]  
+    mov bl, [num2]  
+    mul bl      
+
+
+    mov bl, 10     
+    div bl  
+
+    add al, '0'   
+    add ah, '0'    
+
+    mov [resultado], al    
+    mov [resultado+1], ah  
+    mov byte [resultado+2], 0xA 
+
+
+    mov eax, 4      
+    mov ebx, 1     
+    mov ecx, msg
+    mov edx, len_msg
     int 0x80
 
-    ; Mostrar resultado
+
     mov eax, 4
-    mov ecx, resultado 
-    mov edx, 2      ; 1 byte para el dígito + 1 para el salto de línea
+    mov ecx, resultado
+    mov edx, 3     
     int 0x80
 
-    ; Salir
-    mov eax, 1      ; sys_exit
-    mov ebx, 0      ; código de salida
+
+    mov eax, 1   
+    mov ebx, 0
     int 0x80
